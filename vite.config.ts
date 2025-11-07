b447a86c9734c950a786c0dc6d592e5649a14075
Brian Leach Christensen <blc@elcoyote.dk>
2025-11-07 07:47:37 +0100

    Fix GitHub Pages environment name formatting and set base path in Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
+  base: '/synth.js/',
   server: {
     port: 3000,
     open: true,
